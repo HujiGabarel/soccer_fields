1e24b50020a9af4d4386f8a6c11160d1258ab323
Guy Harel <guy.harel@mail.huji.ac.il>
2022-12-19 16:26:46 +0200

    clean and document the predict class

--- predict_with_trained_model.py
+++ predict_with_trained_model.py
@@ -5,25 +5,21 @@ import rasterio as rio
 from rasterio import plot
 from os import path
 """
-This file load trained model and predict for given image
+Load an image and a trained model, predict which pixels in the image are trees (yellow), and non trees (purple)
 """
-# load the trained model
-trained_model_name='0.joblib'
+
+trained_model_name = 'clf.joblib'
+image_to_predict = "Forest Segmented/some_images_tif\\3484_sat_23.tif"
+
 trained_model = load(trained_model_name)
 
-# chose image
-image_to_predict="Forest Segmented/some_images_tif\\3484_sat_23.tif" # path to image to predict
-
+#predict trees and non-trees
 y_pred = dtr.Classifier().classify_img(image_to_predict, trained_model)
 
-# plots predicted and original images
 # side-by-side plot of the tile and the predicted tree/non-tree pixels
 figwidth, figheight = plt.rcParams['figure.figsize']
 fig, axes = plt.subplots(1, 2, figsize=(2 * figwidth, figheight))
-
-# with rio.open(img_filepath) as src:
 with rio.open(image_to_predict) as src:
     plot.show(src.read(), ax=axes[0])
-axes[1].set_title(image_to_predict)
 axes[1].imshow(y_pred)
 plt.show()
