32f7123b621d0fbe8c51491d3c1f76984d7958cf
Guy Harel <guy.harel@mail.huji.ac.il>
2022-12-19 13:14:46 +0200

    minor changes, tests

--- obtain_data.py
+++ obtain_data.py
@@ -23,10 +23,15 @@ def make_tif_from_images(image_and_paths, is_masks, dest_folder):
             # NOTICE: this is correct only for the database that we have where each mask is tagged the same as
             # the image only "mask" instead of "sat"
             name = name[:name.find(MASK_PREFIX)] + IMG_PREFIX + name[name.find(MASK_PREFIX) + len(MASK_PREFIX):]
-        cv2.imwrite(dest_folder + '_tif/' + name[:-3] + 'tif', img)
+        cv2.imwrite(dest_folder + name[:-3] + 'tif', img)
 
 
 def load_images_from_folder(folder):
+    """
+    gets all the images from a folder into a list
+    :param folder: folder path
+    :return: images paired with their filename
+    """
     images = []
     for filename in os.listdir(folder):
         img = cv2.imread(os.path.join(folder, filename))
@@ -36,17 +41,17 @@ def load_images_from_folder(folder):
 
 
 def black_and_whitefy(img):
-    '''
+    """
     Takes an image and makes it black and white (only 0 or 255)
     :param img: RGB image
     :return: 0 or 255 image
-    '''
+    """
     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     (thresh, black_and_white_image) = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
     return black_and_white_image
 
 
 if __name__ == '__main__':
-    source = "Forest Segmented/some_masks"
+    source = "Forest Segmented/some_images"
     dest = source + '_tif/'
-    make_tif_from_images(load_images_from_folder(source), True, dest)
+    make_tif_from_images(load_images_from_folder(source), False, dest)
