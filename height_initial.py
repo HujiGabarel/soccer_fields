feae34cfffd5427b3e123dbd4723733485b56854
Guy Harel <guy.harel@mail.huji.ac.il>
2023-01-12 14:56:34 +0200

    initial heatmap ideas

--- height_initial.py
+++ height_initial.py
@@ -1,4 +1,7 @@
 import rasterio
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 # Reading the DEM using rasterio
 dem = rasterio.open("Forest Segmented/Yokneam.tif")
@@ -9,7 +12,42 @@ print(rows, cols)
 print(dem.crs)
 print(dem.bounds)
 
+
+
+def find_differnces_in_neightboring_nodes(dem_data, rows, cols):
+    dem_horizontal = []
+    dem_vertical = []
+    for i in range(rows - 1):
+        row_horizontal = []
+        row_vertical = []
+        for j in range(cols - 1):
+            row_horizontal.append(abs(dem_data[i + 1][j] - dem_data[i][j]))
+            row_vertical.append(abs(dem_data[i][j + 1] - dem_data[i][j]))
+        dem_horizontal.append(row_horizontal)
+        dem_vertical.append(row_vertical)
+    return dem_horizontal, dem_vertical
+
+a, b = find_differnces_in_neightboring_nodes(dem_data, rows, cols)
+
+
+def add_2d_array_elementwise(a, b):
+    for i in range(len(a)):
+        for j in range(len(a[0])):
+            a[i][j] += b[i][j]
+    return a
+
+def heat_map(a, b):
+    c = add_2d_array_elementwise(a, b)
+    plt.imshow(c)
+    plt.show()
+
+
+heat_map(a, b)
+
+
 # Creating empty list to store arrays of values
+
+
 arr = list()
 
 # Loop through every cell of the raster
