e3a329552b93255a9bb5ee3ae8d028adc65041e2
Matan <matan.hadad1@mail.huji.ac.il>
2023-01-16 21:47:26 +0200

    commit train_model_automatically.pymodel

--- Modules/Trees/train_model_automatically.py
+++ Modules/Trees/train_model_automatically.py
@@ -6,42 +6,44 @@ from os import path
 from joblib import dump, load
 
 
-def create_model_and_test_image(image_directory, mask_directory):
+def create_model_and_test_image(image_directory, mask_directory, percentage_for_train):
     """
     train model on a given data from directories and then polt test image
     :param image_directory: name of directory - string
     :param mask_directory: name of directory - string
-    :return:
+    :param percentage_for_train: number between 0 and 100
+
+    :return: None
     """
     # select the training tiles from the tiled aerial imagery dataset
     ts = dtr.TrainingSelector(img_dir='Forest Segmented/' + image_directory)
-    split_df = ts.train_test_split(method='cluster-I',train_prop=0.98)
-    dump(split_df, 'Models/split_df_with_98%.joblib')
-
+    split_df = ts.train_test_split(method='cluster-I', train_prop=(percentage_for_train / 100))
+    # We save split_df data because we want to know which files we need to use for the testing (in predict file)
+    dump(split_df, 'Models/split_df_with_' + percentage_for_train + '%.joblib')
     # train a tree/non-tree pixel classfier
     clf = dtr.ClassifierTrainer().train_classifier(
         split_df=split_df, response_img_dir='Forest Segmented/' + mask_directory)  # mask
 
     # save the model to 'trained_model.joblib'
-    dump(clf, 'Models/trained_model_with_98%.joblib')
+    dump(clf, 'Models/trained_model_with_' + percentage_for_train + '%.joblib')
 
-    # use the trained classifier to predict the tree/non-tree pixels
-    take_images_not_trained = split_df.loc[~split_df['train']]
-    test_filepath = take_images_not_trained.sample(1).iloc[0]['img_filepath']  # chose the filepath for test
-    # test_filepath = split_df[~split_df['train'].sample(1).iloc[0]['img_filepath']] # iloc - take row i, sample - take one random
-    y_pred = dtr.Classifier().classify_img(test_filepath, clf)  # prediect using the model
-
-    # side-by-side plot of the tile and the predicted tree/non-tree pixels
-    figwidth, figheight = plt.rcParams['figure.figsize']
-    fig, axes = plt.subplots(1, 2, figsize=(2 * figwidth, figheight))
-
-    # with rio.open(img_filepath) as src:
-    with rio.open(test_filepath) as src:
-        plot.show(src.read(), ax=axes[0])
-    axes[1].set_title(test_filepath)
-    axes[1].imshow(y_pred)
-    plt.show()
+    # This part is irrelevant because we are doing this part in predict file.
+    # # use the trained classifier to predict the tree/non-tree pixels
+    # take_images_not_trained = split_df.loc[~split_df['train']]
+    # test_filepath = take_images_not_trained.sample(1).iloc[0]['img_filepath']  # chose the filepath for test
+    # # test_filepath = split_df[~split_df['train'].sample(1).iloc[0]['img_filepath']] # iloc - take row i, sample - take one random
 
 
 if __name__ == '__main__':
-    create_model_and_test_image('all_images_tif', 'all_masks_tif')
+    """
+    In this file, we enter the images, their masks, and the percentage for the train.
+    After the training is finished, we save a trained model. 
+    We also save the images that are going to be for the testing (in split_df)
+    """
+
+    image_directory = 'all_images_tif'
+    mask_directory = 'all_masks_tif'
+    # need to add the file check!
+
+    percentage_for_train = 98  # Decide according to what percentage of the information we want to train.
+    create_model_and_test_image(image_directory, mask_directory, percentage_for_train)
