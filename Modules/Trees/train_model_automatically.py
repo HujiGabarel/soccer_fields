7e299635095546809151e8ec91c7a868d468b1fb
Owner <matan.hadad.1@mail.huji.ac.il>
2023-01-17 19:24:02 +0200

    train_model_automatically.py is the best now for real real

--- Modules/Trees/train_model_automatically.py
+++ Modules/Trees/train_model_automatically.py
@@ -13,7 +13,6 @@ def create_model_and_test_image(image_directory, mask_directory, percentage_for_
     :param image_directory: name of directory - string
     :param mask_directory: name of directory - string
     :param percentage_for_train: number between 0 and 100
-
     :return: None
     """
     # select the training tiles from the tiled aerial imagery dataset
@@ -21,13 +20,13 @@ def create_model_and_test_image(image_directory, mask_directory, percentage_for_
     split_df = ts.train_test_split(method='cluster-I', train_prop=(percentage_for_train / 100))
     # We save split_df data because we want to know which files we need to use for the testing (in predict file)
     directory_name = path_to_directory_name(image_directory)
-    dump(split_df, 'Models/split_df_of_'+directory_name+'with_' + percentage_for_train + '%.joblib')
+    dump(split_df, 'Models/split_df_of_'+directory_name+'with_' + str(percentage_for_train) + '%.joblib')
     # train a tree/non-tree pixel classfier
     clf = dtr.ClassifierTrainer().train_classifier(
         split_df=split_df, response_img_dir=mask_directory)  # mask
 
     # save the model to 'trained_model_of_xxxx_with_xx%.joblib'
-    dump(clf, 'Models/trained_model_of_'+directory_name+'with_' + percentage_for_train + '%.joblib')
+    dump(clf, 'Models/trained_model_of_'+directory_name+'with_' + str(percentage_for_train) + '%.joblib')
 
     # This part is irrelevant because we are doing this part in predict file.
     # # use the trained classifier to predict the tree/non-tree pixels
@@ -58,10 +57,11 @@ if __name__ == '__main__':
     """
 
     image_directory = '../../Forest Segmented/images_processed'
-    mask_directory = '../../Forest Segmented/some_masks_tif'
+    mask_directory = '../../Forest Segmented/masks_processed'
     images_without_mask = check_all_images_have_mask(image_directory, mask_directory)
     if images_without_mask == "":
-        percentage_for_train = 10  # Decide according to what percentage of the information we want to train.\
+        print("Start Training")
+        percentage_for_train = 50  # Decide according to what percentage of the information we want to train.
         create_model_and_test_image(image_directory, mask_directory, percentage_for_train)
     else:
         raise Exception("There is no mask for these photos: " + images_without_mask)
