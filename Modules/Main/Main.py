b4ff135992e991102a4433e1fc7a9769a68aaed9
Guy Harel <guy.harel@mail.huji.ac.il>
2023-03-22 12:29:11 +0200

    Added main module

---
+++ Modules/Main/Main.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def get_image_from_utm(coordinates):
+    """Returns an image from the coordinates"""
+    pass
+
+
+def get_tree_mask_from_image(aerial):
+    pass
+
+
+def get_partial_dtm_from_utm(coordinates):
+    pass
+
+
+def get_total_mask_from_masks(tree_mask, heights_mask):
+    pass
+
+
+def get_heights_mask_from_dtm(dtm):
+    pass
+
+
+if __name__ == '__main__':
+    coordinates = (0,0)
+    aerial = get_image_from_utm(coordinates)
+    tree_mask = get_tree_mask_from_image(aerial)
+    dtm = get_partial_dtm_from_utm(coordinates)
+    heights_mask = get_heights_mask_from_dtm(dtm)
+    total_mask = get_total_mask_from_masks(tree_mask, heights_mask)
+    plt.imshow(total_mask)
