65d7028fc6ec05cd9025cd0a456fea6d68d85f68
Guy Harel <guy.harel@mail.huji.ac.il>
2023-03-22 13:02:39 +0200

    Added skeleton for radius input

--- Modules/Main/Main.py
+++ Modules/Main/Main.py
@@ -23,11 +23,19 @@ def get_heights_mask_from_dtm(dtm):
     pass
 
 
+def get_viable_landing_in_radius(coordinates, radius = 1):
+    # LOOP OVER ALL THE COORDINATES IN THE RADIUS
+    #     aerial = get_image_from_utm(coordinates)
+    #     tree_mask = get_tree_mask_from_image(aerial)
+    #     dtm = get_partial_dtm_from_utm(coordinates)
+    #     heights_mask = get_heights_mask_from_dtm(dtm)
+    #     total_mask = get_total_mask_from_masks(tree_mask, heights_mask)
+    #     plt.imshow(total_mask)
+    pass
+
+
 if __name__ == '__main__':
-    coordinates = (0,0)
-    aerial = get_image_from_utm(coordinates)
-    tree_mask = get_tree_mask_from_image(aerial)
-    dtm = get_partial_dtm_from_utm(coordinates)
-    heights_mask = get_heights_mask_from_dtm(dtm)
-    total_mask = get_total_mask_from_masks(tree_mask, heights_mask)
-    plt.imshow(total_mask)
+    coordinates = (0, 0)
+    get_viable_landing_in_radius(coordinates)
+
+
