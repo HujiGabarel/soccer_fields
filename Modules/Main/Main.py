870b0e51951c88ee9dc5b55fdae614e3879be0cd
Guy Harel <guy.harel@mail.huji.ac.il>
2023-03-23 08:48:28 +0200

    Documentation for main

--- Modules/Main/Main.py
+++ Modules/Main/Main.py
@@ -3,23 +3,32 @@ import numpy as np
 
 
 def get_image_from_utm(coordinates):
-    """Returns an image from the coordinates"""
+    # Returns an image from the coordinates with the SIZE * SIZE size
     pass
 
 
 def get_tree_mask_from_image(aerial):
+    # using trees module, get the mask of the trees in black white
     pass
 
 
 def get_partial_dtm_from_utm(coordinates):
+    # find the area in the dtm that is relevant
+    # cut around the area in a SIZE*SIZE matrix
     pass
 
 
 def get_total_mask_from_masks(tree_mask, heights_mask):
+    # return tree_mask * heights_mask
     pass
 
 
-def get_heights_mask_from_dtm(dtm):
+def get_slopes_mask_from_dtm(dtm):
+    # get heights from the dtm
+    # get slopes from the heights
+    # get mask from the slopes
+    # return mask
+    #TODO: SIZE of the image is constant?
     pass
 
 
@@ -28,7 +37,7 @@ def get_viable_landing_in_radius(coordinates, radius = 1):
     #     aerial = get_image_from_utm(coordinates)
     #     tree_mask = get_tree_mask_from_image(aerial)
     #     dtm = get_partial_dtm_from_utm(coordinates)
-    #     heights_mask = get_heights_mask_from_dtm(dtm)
+    #     heights_mask = get_slopes_mask_from_dtm(dtm)
     #     total_mask = get_total_mask_from_masks(tree_mask, heights_mask)
     #     plt.imshow(total_mask)
     pass
