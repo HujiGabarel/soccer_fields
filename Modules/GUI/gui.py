e3fdf35a30b4d4f13bcea4cc7d94b8a79ca156f8
Matan <matan.hadad1@mail.huji.ac.il>
2023-04-16 15:16:26 +0300

    update gui design

--- Modules/GUI/gui.py
+++ Modules/GUI/gui.py
@@ -4,23 +4,29 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import tkinter as tk
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageSequence
 import numpy as np
 from Modules.Main.Main import get_viable_landing_in_radius
 import cv2
 import threading
 import os
+from PIL import Image
 
 # Get the directory path of the current file (module.py)
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 # Construct the absolute path of logo.png
+logo_path_gif = os.path.join(dir_path, 'LOGO.gif')
 logo_path = os.path.join(dir_path, 'logo.png')
+FONT = ('Helvetica', 16, "bold")
+background_color = 'white'
+second_background_color = 'light blue'
 
 
 class GUI(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.configure(background=background_color)
         self.search_button = None
         self.Radius_entry = None
         self.Radius = None
@@ -33,8 +39,8 @@ class GUI(tk.Tk):
         self.Radius_value = None
         self.title("Soccer Field GUI:")
         # put a picture on the window background
-        self.x = 800
-        self.y = 600
+        self.x = 1000
+        self.y = 1000
         flag = False
         if flag:
             self.geometry(f"{self.x}x{self.y}")
@@ -44,8 +50,8 @@ class GUI(tk.Tk):
             self.y = 500
             self.state("zoomed")
         picture_x_y = 150
-        self.background_image = ImageTk.PhotoImage(Image.open(logo_path).resize((picture_x_y, picture_x_y)))
-        self.resizable(False, False)
+        # self.add_background_gif()
+        self.resizable(True, True)
         self.create_widgets()
 
     def create_widgets(self):
@@ -55,62 +61,64 @@ class GUI(tk.Tk):
         self.E = tk.Label(self, text="E: ")
         self.E.pack()
         self.E_entry = tk.Entry(self)
+        self.E_entry.config(background=second_background_color)
         self.E_entry.pack()
         self.N = tk.Label(self, text="N: ")
         self.N.pack()
         self.N_entry = tk.Entry(self)
+        self.N_entry.config(background=second_background_color)
         self.N_entry.pack()
         self.Radius = tk.Label(self, text="Radius in km: ")
         self.Radius.pack()
         self.Radius_entry = tk.Entry(self)
+        self.Radius_entry.config(background=second_background_color)
         self.Radius_entry.pack()
 
         # inserting the entry values into the search function
-        # self.search_button = tk.Button(self, text="חפש", command=self.do)
-        self.search_button = tk.Button(self, text="חפש", command=threading.Thread(target=self.do).start)
+        self.search_button = tk.Button(self, text="חפש", command=self.do)
+        # self.search_button = tk.Button(self, text="חפש", command=threading.Thread(target=self.do).start)
         self.search_button.pack()
         labels = [self.E, self.N, self.Radius, self.E_entry, self.N_entry, self.Radius_entry, self.search_button]
-        start_x = 50
-        gap = 2.6
-        start_y = 20
-        entry_width = 40
+
+        gap = 1.5
+        entry_width = 20
         height = 10
         level = 1
+        start_x = 800
+        start_y = 20
         self.E.place(x=start_x, y=start_y, anchor=position)
-        self.E.config(font=("Arial", 12, "bold"), foreground="black")
+        self.E.config(font=FONT, foreground="black", background=background_color)
         self.E_entry.place(x=50 * gap + start_x, y=start_y, anchor=position)
         self.E_entry.config(width=entry_width)
         self.N.place(x=100 * gap + start_x, y=start_y, anchor=position)
-        self.N.config(font=("Arial", 12, "bold"), foreground="black")
+        self.N.config(font=FONT, foreground="black", background=background_color)
         self.N_entry.place(x=150 * gap + start_x, y=start_y, anchor=position)
         self.N_entry.config(width=entry_width)
-        start_x = 70
         self.Radius.place(x=start_x, y=start_y + 50 * level, anchor=position)
-        self.Radius.config(font=("Arial", 12, "bold"), foreground="black")
-        self.Radius_entry.place(x=70 * gap + start_x, y=start_y + 50 * level, anchor=position)
+        self.Radius.config(font=FONT, foreground="black", background=background_color)
+        self.Radius_entry.place(x=100 * gap + start_x, y=start_y + 50 * level, anchor=position)
         self.Radius_entry.config(width=entry_width)
-        self.search_button.place(x=180 * gap + start_x, y=start_y + 50 * level, anchor=position)
-        self.search_button.config(background="green", foreground="white", font=("Arial", 12, "bold"))
-        # put the background image on the window, right side to the buttons
-        self.background_label = tk.Label(self, image=self.background_image)
-        self.background_label.place(x=1400, y=80, anchor=position)
-
+        self.search_button.place(relx=0.5, rely=0.15, anchor=position)
+        self.search_button.config(background=second_background_color, foreground="black", font=FONT)
         self.original_image = Image.open(logo_path)
         self.result_image = Image.open(logo_path)
-        # make self.original_image and self.result_image an array of the image
+        # # make self.original_image and self.result_image an array of the image
         self.original_image_array = np.array(self.original_image)
         self.result_image_array = np.array(self.result_image)
         # add the original image and the result image to the window
         self.add_original_image(self.original_image_array)
-        self.add_result_image(self.result_image_array)
 
+        self.result_image = ImageTk.PhotoImage(
+            Image.fromarray(cv2.cvtColor(self.result_image_array, cv2.COLOR_BGR2RGB)).resize((self.x, self.y)))
+        self.result_label = tk.Label(self, image=self.result_image)
+        self.result_label.place(relx=0.65, rely=0.5, anchor=tk.CENTER)
     def do(self):
         # get the entry values
         self.E_value = self.E_entry.get()
         self.N_value = self.N_entry.get()
         self.Radius_value = self.Radius_entry.get()
         coordinates = (int(self.E_value), int(self.N_value), 36, "N")
-        print(coordinates,self.Radius_value)
+        print(coordinates, self.Radius_value)
         # run the function
         image, total_mask = get_viable_landing_in_radius(coordinates, float(self.Radius_value))
         self.add_original_image(image)
@@ -122,9 +130,10 @@ class GUI(tk.Tk):
         :param image: the original image
         :return:
         """
-        self.original_image = ImageTk.PhotoImage(Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)).resize((self.x, self.y)))
+        self.original_image = ImageTk.PhotoImage(
+            Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)).resize((self.x, self.y)))
         self.original_label = tk.Label(self, image=self.original_image)
-        self.original_label.place(x=self.x , y=self.y/ 1.5+100, anchor=tk.CENTER)
+        self.original_label.place(relx=0.35, rely=0.5, anchor=tk.CENTER)
 
     def add_result_image(self, image):
         """
@@ -134,7 +143,7 @@ class GUI(tk.Tk):
         """
         self.result_image = ImageTk.PhotoImage(Image.fromarray(image).resize((self.x, self.y)))
         self.result_label = tk.Label(self, image=self.result_image)
-        self.result_label.place(x=self.x  * 2 + 20, y=self.y /1.5+100, anchor=tk.CENTER)
+        self.result_label.place(relx=0.6, rely=0.5, anchor=tk.CENTER)
 
     def get_E_value(self):
         return self.E_value
@@ -144,6 +153,29 @@ class GUI(tk.Tk):
 
     def get_Radius_value(self):
         return self.Radius_value
+
+    def add_background_gif(self):
+        # Open the GIF image using PIL
+        image = Image.open(logo_path_gif)  # Replace "animation.gif" with the filename or file path of your GIF image
+        # Create a list to store individual frames of the GIF image
+        frames = []
+        # Loop through each frame in the GIF image and append it to the frames list
+        for frame in ImageSequence.Iterator(image):
+            resized_frame = frame.resize((150, 150))
+            frames.append(ImageTk.PhotoImage(resized_frame))
+        # Create a Label widget to display the animated GIF
+        self.background_label = tk.Label(self)
+        self.background_label.place(x=1800, y=300, anchor=tk.CENTER)
+        self.background_label.pack()
+
+        # self.background_label.resize(100, 100)
+        # Function to update the Label with the next frame of the animated GIF
+        def update_label(frame_idx):
+            self.background_label.config(image=frames[frame_idx])
+            self.after(100, update_label, (frame_idx + 1) % len(frames))
+
+        # Start displaying the animated GIF
+        update_label(0)
 
 
 # Press the green button in the gutter to run the script.
