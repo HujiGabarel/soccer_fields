fc060107d325a57889c508b906d5e74cdbec1220
Matan <matan.hadad1@mail.huji.ac.il>
2023-04-22 19:39:18 +0300

    clean code and change selector of type (need to rearange more)

--- Modules/GUI/gui.py
+++ Modules/GUI/gui.py
@@ -21,7 +21,7 @@ pnotnp_path = os.path.join(dir_path, 'PNP.jpg')
 # Construct the absolute path of background.png
 pnp_path = os.path.join(dir_path, 'PNOTNP.jpg')
 # Construct the absolute path of background.png
-ws_path = os.path.join(dir_path, 'whitesquare.png')
+ws_path = os.path.join(dir_path, 'logo.png')
 FONT = ('Helvetica', 16, "bold")
 background_color = 'white'
 second_background_color = 'light blue'
@@ -31,16 +31,6 @@ class GUI(tk.Tk):
     def __init__(self):
         super().__init__()
         self.configure(background=background_color)
-        self.search_button = None
-        self.Radius_entry = None
-        self.Radius = None
-        self.N_entry = None
-        self.N = None
-        self.E_entry = None
-        self.E = None
-        self.E_value = None
-        self.N_value = None
-        self.Radius_value = None
         self.title("Soccer Field GUI:")
         # put a picture on the window background
         self.x = 1000
@@ -60,10 +50,7 @@ class GUI(tk.Tk):
         self.resizable(True, True)
         self.create_widgets()
 
-    def create_widgets(self):
-        # place the buttons on the window with a nice layout
-        # add an entry for entering, E, N, and S, insert the entry values into the search function
-        position = tk.CENTER
+    def create_inputs_cells(self):
         self.E = tk.Label(self, text="E: ")
         self.E.pack()
         self.E_entry = tk.Entry(self)
@@ -80,35 +67,30 @@ class GUI(tk.Tk):
         self.Radius_entry.config(background=second_background_color, foreground="black", font=FONT, justify=tk.CENTER)
         self.Radius_entry.pack()
 
-        # inserting the entry values into the search function
-        self.search_button = tk.Button(self, text="חפש", command=self.search)
-        # self.search_button = tk.Button(self, text="חפש", command=threading.Thread(target=self.do).start)
-        self.search_button.pack()
-        self.add_progressbar()
-        labels = [self.E, self.N, self.Radius, self.E_entry, self.N_entry, self.Radius_entry, self.search_button]
-
-        gap = 1.5
         entry_width = 20
-        height = 10
-        level = 1
-        start_x = 800
-        start_y = 20
-        self.E.place(relx=0.41, rely=0.05, anchor=position)
+        self.E.place(relx=0.41, rely=0.05, anchor=tk.CENTER)
         self.E.config(font=FONT, foreground="black", background=background_color)
-        self.E_entry.place(relx=0.5, rely=0.05, anchor=position)
+        self.E_entry.place(relx=0.5, rely=0.05, anchor=tk.CENTER)
         self.E_entry.config(width=entry_width)
-        self.N.place(relx=0.41, rely=0.1, anchor=position)
+        self.N.place(relx=0.41, rely=0.1, anchor=tk.CENTER)
         self.N.config(font=FONT, foreground="black", background=background_color)
-        self.N_entry.place(relx=0.5, rely=0.1, anchor=position)
+        self.N_entry.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
         self.N_entry.config(width=entry_width)
-        self.Radius.place(relx=0.375, rely=0.15, anchor=position)
+        self.Radius.place(relx=0.375, rely=0.15, anchor=tk.CENTER)
         self.Radius.config(font=FONT, foreground="black", background=background_color)
-        self.Radius_entry.place(relx=0.5, rely=0.15, anchor=position)
+        self.Radius_entry.place(relx=0.5, rely=0.15, anchor=tk.CENTER)
         self.Radius_entry.config(width=entry_width)
-        self.search_button.place(relx=0.5, rely=0.2, anchor=position)
-        self.search_button.config(background=second_background_color, foreground="black", font=FONT)
-        self.original_image = Image.open(pnp_path)
-        self.result_image = Image.open(pnotnp_path)
+
+    def create_widgets(self):
+        # place the buttons on the window with a nice layout
+        # add an entry for entering, E, N, and S, insert the entry values into the search function
+        self.create_inputs_cells()
+        # inserting the entry values into the search function
+        self.add_search_button()
+        self.add_progressbar()
+
+        self.original_image = Image.open(logo_path)
+        self.result_image = Image.open(pnp_path)
         # # make self.original_image and self.result_image an array of the image
         self.original_image_array = np.array(self.original_image)
         self.result_image_array = np.array(self.result_image)
@@ -119,12 +101,14 @@ class GUI(tk.Tk):
         self.add_original_image(self.original_image_array)
 
         # self.add_result_image(self.result_image_array)
-        self.result_image_1 = Image.fromarray(cv2.cvtColor(self.result_image_array, cv2.COLOR_BGR2RGB)).resize((self.x, self.y))
+        self.result_image_1 = Image.fromarray(cv2.cvtColor(self.result_image_array, cv2.COLOR_BGR2RGB)).resize(
+            (self.x, self.y))
         self.result_image = ImageTk.PhotoImage(self.result_image_1)
-        self.square_image_array = np.array(Image.open(ws_path))
-        self.square_image_1 = Image.fromarray(cv2.cvtColor(self.square_image_array, cv2.COLOR_BGR2RGB)).resize((self.sx, self.sy))
-        self.square_image = ImageTk.PhotoImage(self.square_image_1)
 
+        self.square_image_array = np.array(Image.open(ws_path))
+        self.square_image_1 = Image.fromarray(cv2.cvtColor(self.square_image_array, cv2.COLOR_BGR2RGB)).resize(
+            (self.sx, self.sy))
+        self.square_image = ImageTk.PhotoImage(self.square_image_1)
 
         self.result_image_canvas = self.canvas.create_image(0, 0, image=self.result_image, anchor=tk.NW)
         self.square_image_canvas = self.canvas.create_image(0, 0, image=self.square_image, anchor=tk.NW)
@@ -137,39 +121,10 @@ class GUI(tk.Tk):
         self.bind("1", self.rotate_square_image_clockwise)
         # self.canvas.bind("<B1-Motion>", self.rotate_image)
         # Create a slider to control the transparency of the image
-        transparency_slider = tk.Scale(self, from_=0, to=100, orient=tk.VERTICAL, length=450, width=15, sliderlength=20, background=background_color, foreground="black", )
-        transparency_slider.set(100)
-        transparency_slider.pack()
-        transparency_slider.place(relx=0.32, rely=0.55, anchor=tk.CENTER)
-        # Call the update_transparency function when the slider is moved
-        transparency_slider.config(command=self.update_transparency)
-        self.update_transparency(100)
-        # add a button which will let the user to choose the size of the square image from a listbox of options
-        self.size_button = tk.Button(self, text="Change Type", command=self.size)
-        self.size_button.pack()
-        self.size_button.place(relx=0.2, rely=0.7, anchor=tk.CENTER)
-        self.size_button.config(background=second_background_color, foreground="black", font=FONT)
-        # when an item from the listbox is selected, the value of the selected item will be saved in the self.selected variable
-        self.selected = tk.StringVar()
-        # change the names to the names of the helicopter types
-        self.selected.set("Small")
-        options = ["Small", "Medium"]
-        self.listbox = tk.OptionMenu(self, self.selected, *options)
-        self.listbox.pack()
-        self.listbox.place(relx=0.2, rely=0.6, anchor=tk.CENTER)
-        self.listbox.config(background=second_background_color, foreground="black", font=FONT)
-
-
-    def size(self):
-        # change the size of the square image to whatever you want
-        val = self.selected.get()
-        if val == "Small":
-            self.sx = 50
-            self.sy = 20
-        elif val == "Medium":
-            self.sx = 70
-            self.sy = 30
-        self.add_square_image()
+        self.create_slider()
+        # # add a button which will let the user to choose the size of the square image from a listbox of options
+        self.create_type_label()
+        self.change_type_button()
 
     def update_transparency(self, val):
         alpha = int((100 - float(val)) * 255 / 100)
@@ -178,6 +133,7 @@ class GUI(tk.Tk):
         alpha_image = ImageTk.PhotoImage(cop)
         self.canvas.itemconfig(self.result_image_canvas, image=alpha_image)
         self.canvas.image = alpha_image
+
     def rotate_image(self, event):
         # Calculate the angle of rotation based on the change in mouse position
         dx = event.x - self.x / 2
@@ -192,9 +148,11 @@ class GUI(tk.Tk):
     def rotate_square_image_clockwise(self, event):
         # rotate the square image 45 degrees clockwise
         self.sx, self.sy = self.sy, self.sx
-        self.square_image_1 = Image.fromarray(cv2.cvtColor(self.square_image_array, cv2.COLOR_BGR2RGB)).resize((self.sx, self.sy))
+        self.square_image_1 = Image.fromarray(cv2.cvtColor(self.square_image_array, cv2.COLOR_BGR2RGB)).resize(
+            (self.sx, self.sy))
         self.square_image = ImageTk.PhotoImage(self.square_image_1)
         self.canvas.itemconfig(self.square_image_canvas, image=self.square_image)
+
     def left_key(self, event):
         # dont let the square image go out of the canvas\
         c1 = self.canvas.coords(self.square_image_canvas)
@@ -206,8 +164,6 @@ class GUI(tk.Tk):
         c1 = self.canvas.coords(self.square_image_canvas)
         if c1[0] + self.sx < self.x:
             self.canvas.move(self.square_image_canvas, 1, 0)
-
-
 
     def up_key(self, event):
         # dont let the square image go out of the canvas
@@ -250,6 +206,7 @@ class GUI(tk.Tk):
             (self.sx, self.sy))
         self.square_image = ImageTk.PhotoImage(self.square_image_1)
         self.square_image_canvas = self.canvas.create_image(0, 0, image=self.square_image, anchor=tk.NW)
+
     def add_original_image(self, image):
         """
         get the original image in the form of array and put it on the window bellow the buttons, resize it to fit the x but the y should be 200
@@ -274,7 +231,6 @@ class GUI(tk.Tk):
         # self.result_label.place(relx=0.7, rely=0.5, anchor=tk.CENTER)
         self.result_image_canvas = self.canvas.create_image(0, 0, image=self.result_image, anchor=tk.NW)
         self.add_square_image()
-
 
     def get_E_value(self):
         return self.E_value
@@ -322,9 +278,46 @@ class GUI(tk.Tk):
         # Start displaying the animated GIF
         update_label(0)
 
+    def add_search_button(self):
+        self.search_button = tk.Button(self, text="חפש", command=self.search)
+        self.search_button.pack()
+        self.search_button.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
+        self.search_button.config(background=second_background_color, foreground="black", font=FONT)
+
+    def create_slider(self):
+        transparency_slider = tk.Scale(self, from_=0, to=100, orient=tk.VERTICAL, length=450, width=15, sliderlength=20,
+                                       background=background_color, foreground="black", )
+        transparency_slider.set(100)
+        transparency_slider.pack()
+        transparency_slider.place(relx=0.32, rely=0.55, anchor=tk.CENTER)
+        # Call the update_transparency function when the slider is moved
+        transparency_slider.config(command=self.update_transparency)
+        self.update_transparency(100)
+
+
+    def change_type_button(self):
+        self.size_button = tk.Button(self, text="Change Type", command=self.update_size)
+        self.size_button.pack()
+        self.size_button.place(relx=0.2, rely=0.7, anchor=tk.CENTER)
+        self.size_button.config(background=second_background_color, foreground="black", font=FONT)
+    def create_type_label(self):
+        self.type_label = tk.Label(self, text="יסעור", font=FONT, foreground="black", background=second_background_color)
+        self.type_label.place(relx=0.2, rely=0.6, anchor=tk.CENTER)
+
+    def update_size(self):
+        # change the size of the square image to fit the helicopter type
+        if self.type_label["text"] == "יסעור":
+            self.type_label["text"]="ינשוף"
+            self.sx = 50
+            self.sy = 20
+        elif self.type_label["text"] == "ינשוף":
+            self.type_label["text"]="יסעור"
+            self.sx = 70
+            self.sy = 200
+        self.add_square_image()
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     gui = GUI()
     gui.mainloop()
-    print("Hello world")
