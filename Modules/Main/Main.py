7ed51384fa7972199b97d196eca0d38b3c982e6c
Guy Harel <guy.harel@mail.huji.ac.il>
2023-05-21 14:04:44 +0300

    added typing for main, and some docstrings

--- Modules/Main/Main.py
+++ Modules/Main/Main.py
@@ -1,48 +1,51 @@
-# from importing from root directory.
 import sys
-
-sys.path.append('../..')
-
+from typing import List, Tuple, Dict
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import json
-import Modules.Main.image_downloading as image_downloading
 import rasterio
 import cv2
 import rasterio as rio
 from rasterio import plot
-from Modules.Trees.predict_with_trained_model import predict_image
-from Modules.Slopes.slopes import get_max_slopes, plot_heat_map, convert_slopes_to_black_and_white
-# from Modules.Building.predict import detect_building, convert_building_to_black_white, convert_building_to_tree_style
-from Modules.GUI import gui
-from Modules.AreaFilter.Filterspecks import FilterSpecks
-from Modules.AreaFilter.RectangleFilter import detect_rectangles
 import math
 import openpyxl
 import time
 
+# Adding the root directory to the system path
+sys.path.append('../..')
+
+# Modules from your project
+import Modules.Main.image_downloading as image_downloading
+from Modules.Trees.predict_with_trained_model import predict_image
+from Modules.Slopes.slopes import get_max_slopes, plot_heat_map, convert_slopes_to_black_and_white
+from Modules.GUI import gui
+from Modules.AreaFilter.Filterspecks import FilterSpecks
+from Modules.AreaFilter.RectangleFilter import detect_rectangles
+
+# Constants
 DTM_FILE_PATH = "../../DTM_data/DTM_new/dtm_mimad_wgs84utm36_10m.tif"
 trained_model_path = "../../Models/our_models/official_masks_10%.joblib"  # The trained model
 
 
 # TODO: decide about length
-def get_image_from_utm(coordinates, km_radius):
+def get_image_from_utm(coords: Tuple[float, float], km_radius: float) -> Tuple[str, np.ndarray]:
     with open("preferences.json", 'r', encoding='utf-8') as f:
         prefs = json.loads(f.read())
-    lat, long = image_downloading.convert_to_lat_long(coordinates)
+    lat, long = image_downloading.convert_to_lat_long(coords)
     print(lat, long)
     img = image_downloading.download_image(lat, long, prefs["zoom"], prefs['url'], prefs['tile_size'],
                                            length=2 * km_radius)
     print("image downloaded")
     # timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    name = f'images_from_argcis/img_{coordinates[0], coordinates[1]}.png'
+    name = f'images_from_argcis/img_{coords[0], coords[1]}.png'
     cv2.imwrite(name, img)
     print(f'Saved as {name}')
     return name, img
 
 
-def get_building_image_from_utm(coordinates, km_radius):
+# TODO: repetitive code, mix the preferences files
+def get_building_image_from_utm(coordinates: Tuple[float, float], km_radius: float) -> np.ndarray:
     with open("preferences_building.json", 'r', encoding='utf-8') as f:
         prefs = json.loads(f.read())
     lat, long = image_downloading.convert_to_lat_long(coordinates)
@@ -57,34 +60,38 @@ def get_building_image_from_utm(coordinates, km_radius):
     return img
 
 
-def smooth(building_mask):
-    return cv2.morphologyEx(building_mask, cv2.MORPH_OPEN, np.ones((5, 5), np.uint8))
+def smooth(img: np.ndarray) -> np.ndarray:
+    kernel = np.ones((5, 5), np.uint8)
+    return cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
 
 
-def building_image_to_building_mask(building_image):
-    # run over each pixel and check if it is a in range of rgb of [220, 220, 220]
-    # if it is, change it to 1, else change it to 0
-    # return the mask
-    # 255 is building, 0 is not building
-    range_fiter = range(210, 225)
-    building_mask = np.copy(building_image)
-    for i in range(building_mask.shape[0]):
-        for j in range(building_mask.shape[1]):
-            if building_mask[i][j][0] in range_fiter and building_mask[i][j][1] in range_fiter and building_mask[i][j][
-                2] in range_fiter:
-                building_mask[i][j] = 255
-            else:
-                building_mask[i][j] = 0
-    # convert to grayscale
-    return cv2.cvtColor(smooth(building_mask), cv2.COLOR_BGR2GRAY)
+def building_image_to_building_mask(building_image: np.ndarray) -> np.ndarray:
+    '''
+    Get the buildings image, return formatted mask of the buildings
+    :param building_image: regular image of the buildings
+    :return: mask of the buildings, 255 is not building, 0 is building
+    '''
+    range_filter = range(210, 225)
+    image = np.copy(building_image)
+    mask = np.all(np.isin(image, range_filter), axis=2)
+    image[mask] = 0
+    image[~mask] = 255
+    return cv2.cvtColor(smooth(image), cv2.COLOR_BGR2GRAY)
 
 
-def get_tree_mask_from_image(aerial, trained_model_path, pixels_to_ignore=[]):
-    # using trees module, get the mask of the trees in black white
+def get_tree_mask_from_image(aerial: np.ndarray, trained_model_path: str, pixels_to_ignore: List[int]) -> np.ndarray:
+    '''
+    Get the aerial image, return formatted mask of the trees
+    :param aerial: aerial image
+    :param trained_model_path: model to predict the trees
+    :param pixels_to_ignore: pixels to automatically ignore, to improve runtime
+    :return: formatted mask of the trees, 255 is not tree, 0 is tree
+    '''
     return predict_image(aerial, trained_model_path, pixels_to_ignore)
 
 
-def check_coordinates_area_are_in_dtm(coordinates, size, dem):
+def check_coordinates_area_are_in_dtm(coordinates: Tuple[float, float], size: float,
+                                      dem: rasterio.DatasetReader) -> bool:
     return ((dem.bounds.left < (coordinates[0] + size) < dem.bounds.right) and
             (dem.bounds.left < (coordinates[0] - size) < dem.bounds.right) and
             (dem.bounds.bottom < (coordinates[1] + size) < dem.bounds.top) and
@@ -92,7 +99,8 @@ def check_coordinates_area_are_in_dtm(coordinates, size, dem):
             )
 
 
-def mask_pixels_from_slopes(slopes_mask_in_black_and_white, tree_shape, slope_shape):
+def mask_pixels_from_slopes(slopes_mask_in_black_and_white: np.ndarray, tree_shape: Tuple[int, int],
+                            slope_shape: Tuple[int, int]) -> np.ndarray:
     unique_values, value_counts = np.unique(slopes_mask_in_black_and_white, return_counts=True)
 
     slope2TreeRow = tree_shape[0] / slope_shape[0]
@@ -110,7 +118,8 @@ def mask_pixels_from_slopes(slopes_mask_in_black_and_white, tree_shape, slope_sh
     return masked_pixels_tree
 
 
-def get_partial_dtm_from_total_dtm(coordinates, km_radius, meters_per_pixel=10):
+def get_partial_dtm_from_total_dtm(coordinates: Tuple[float, float], km_radius: float, meters_per_pixel: float = 10) -> \
+        Tuple[np.ndarray, int, int]:
     # find the area in the dtm that is relevant
     # cut around the area in a SIZE*SIZE matrix
     size = round(km_radius * 1000 / meters_per_pixel)
@@ -143,7 +152,8 @@ def get_partial_dtm_from_total_dtm(coordinates, km_radius, meters_per_pixel=10):
     return partial_dtm, new_rows, new_cols
 
 
-def get_white_or_black(e_vals, n_vals, e_center, n_center, m_radius, trees, slopes):
+def get_white_or_black(e_vals: np.ndarray, n_vals: np.ndarray, e_center: float, n_center: float, m_radius: float,
+                       trees: np.ndarray, slopes: np.ndarray) -> np.ndarray:
     row_tree = np.round((n_vals - n_center + m_radius) / (2 * m_radius) * trees.shape[0]).astype(int)
     col_tree = np.round((e_vals - e_center + m_radius) / (2 * m_radius) * trees.shape[1]).astype(int)
     row_slope = np.round((n_vals - n_center + m_radius) / (2 * m_radius) * slopes.shape[0]).astype(int) - 1
@@ -154,7 +164,8 @@ def get_white_or_black(e_vals, n_vals, e_center, n_center, m_radius, trees, slop
     return np.where(mask, 255, 0)
 
 
-def get_total_mask_from_masks(e_center, n_center, radius, trees, slopes):
+def get_total_mask_from_masks(e_center: float, n_center: float, radius: float, trees: np.ndarray,
+                              slopes: np.ndarray) -> np.ndarray:
     m_radius = int(radius * 1000)
     e_vals = np.arange(e_center - m_radius, e_center + m_radius)
     n_vals = np.arange(n_center - m_radius, n_center + m_radius)
@@ -168,7 +179,8 @@ def get_total_mask_from_masks(e_center, n_center, radius, trees, slopes):
     return total_mask
 
 
-def plot_image_and_mask(image_to_predict, predicted_mask_tree, predicted_mask_slope, total_mask, coordinates):
+def plot_image_and_mask(image_to_predict: str, predicted_mask_tree: np.ndarray, predicted_mask_slope: np.ndarray,
+                        total_mask: np.ndarray, coordinates: Tuple[float, float]) -> None:
     # This part is only plotting style:
     # plots predicted and original images
     # side-by-side plot of the tile and the masks
@@ -188,15 +200,17 @@ def plot_image_and_mask(image_to_predict, predicted_mask_tree, predicted_mask_sl
     plt.show()
 
 
-def update_progressbar_speed(screen_gui, slopes_mask_in_black_and_white):
+def update_progressbar_speed(screen_gui: gui, slopes_mask_in_black_and_white: np.ndarray) -> None:
     count_slopes_good = np.count_nonzero(slopes_mask_in_black_and_white == 255)
     slopy = 100 * count_slopes_good / slopes_mask_in_black_and_white.size
     screen_gui.set_time_for_iteration(slopy)
 
 
-def get_viable_landing_in_radius(coordinates, km_radius, screen_gui):
+def get_viable_landing_in_radius(coordinates: Tuple[float, float], km_radius: float, screen_gui: gui) -> Tuple[
+    np.ndarray, Dict[str, np.ndarray]]:
     st = time.time()
     cputime_start = time.process_time()
+    # TODO: improve modularity, allow user to add or implement more mask functions
     # building mask
     building_image = get_building_image_from_utm(coordinates, km_radius)
     building_mask = building_image_to_building_mask(building_image)
@@ -230,7 +244,7 @@ def get_viable_landing_in_radius(coordinates, km_radius, screen_gui):
     return img, masks_dictionary
 
 
-def data_analyse(slopes_mask_in_black_and_white, km_radius, st, cputime_start):
+def data_analyse(slopes_mask_in_black_and_white: np.ndarray, km_radius: float, st: float, cputime_start: float) -> None:
     # count number of 255 in slopes_mask_in_black_and_white
     count_slopes_good = np.count_nonzero(slopes_mask_in_black_and_white == 255)
     slopy = round(100 * count_slopes_good / slopes_mask_in_black_and_white.size, 2)
@@ -242,7 +256,7 @@ def data_analyse(slopes_mask_in_black_and_white, km_radius, st, cputime_start):
     save_result_to_excel(slopy, area, total_time, cpu_total_time)
 
 
-def save_result_to_excel(slopy, area, total_time, cpu_total_time):
+def save_result_to_excel(slopy: float, area: float, total_time: float, cpu_total_time: float) -> None:
     workbook = openpyxl.load_workbook("results.xlsx")
     worksheet = workbook.active
     worksheet.title = "result"
